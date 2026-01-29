a11ddf78be85d0bfbd95bb3b96afaf34ff69c0a9
Mark Wade <m.wade@me.com>
2026-01-29 22:52:09 +1100

    Fix formatting of US phone numbers

--- src/components/ProfileContactDetails/ProfileContactDetails.tsx
+++ src/components/ProfileContactDetails/ProfileContactDetails.tsx
@@ -118,5 +118,8 @@ function AddressDisplay({ address }: { address: Address }) {
 const countryCodesByName = countryCodes.customList('countryNameEn', '{countryCode}');
 function countryToCountryCode(country: string) {
   const normalisedCountryName = titleCase(country.toLowerCase());
+  if (normalisedCountryName === 'United States') {
+    return 'US'; // It's in the list as "United States of America"
+  }
   return normalisedCountryName in countryCodesByName ? countryCodesByName[normalisedCountryName] : undefined;
 }
